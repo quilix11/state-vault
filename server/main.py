75807826b321ff8ba4668691a2a5a2344b07016e
quilix11 <nazarus8095@gmail.com>
2026-04-13 22:02:10 +0300

    save config backups to database

--- server/main.py
+++ server/main.py
@@ -1,17 +1,25 @@
-import fastapi
+from fastapi import FastAPI,Depends
 from pydantic import BaseModel
 from database.core import engine, Base
+from database.models import ConfigBackup
+from sqlalchemy.orm import Session
+from database.core import get_db
 from database.models import ConfigBackup
 
 Base.metadata.create_all(bind=engine)
 
-app = fastapi.FastAPI()
+app = FastAPI()
 
 class ConfigPayload(BaseModel):
     app_name: str
     config: str
 
 @app.post("/config")
-async def receive_config(payload: ConfigPayload):
+async def receive_config(payload: ConfigPayload, db: Session = Depends(get_db)):
     print(f"Recived config for app '{payload.app_name}':\n{payload.config}")
-    return {"message": f"Config for '{payload.app_name}' received successfully."}
+    backup=ConfigBackup(app_name=payload.app_name, content=payload.config)
+    db.add(backup)
+    db.commit()
+    db.refresh(backup)
+
+    return {"message": f"Config for '{payload.app_name}' received and stored with id {backup.id}."}
