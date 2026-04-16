a45e37206beccb262362a536bc63ac48e56d868f
quilix11 <nazarus8095@gmail.com>
2026-04-16 17:14:17 +0300

    make pull func on server

--- server/app.py
+++ server/app.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI,Depends
+from fastapi import FastAPI,Depends, HTTPException
 from pydantic import BaseModel
 from database.core import engine, Base
 from database.models import ConfigBackup
@@ -14,8 +14,8 @@ class ConfigPayload(BaseModel):
     app_name: str
     config: str
 
-@app.post("/config")
-async def receive_config(payload: ConfigPayload, db: Session = Depends(get_db)):
+@app.post("/push")
+async def push_config(payload: ConfigPayload, db: Session = Depends(get_db)):
     print(f"Recived config for app '{payload.app_name}':\n{payload.config}")
     backup=ConfigBackup(app_name=payload.app_name, content=payload.config)
     db.add(backup)
@@ -23,3 +23,15 @@ async def receive_config(payload: ConfigPayload, db: Session = Depends(get_db)):
     db.refresh(backup)
 
     return {"message": f"Config for '{payload.app_name}' received and stored with id {backup.id}."}
+
+@app.get("/pull/{config_name}")
+async def pull_config(config_name: str, db: Session = Depends(get_db)):
+    config = db.query(ConfigBackup).filter(ConfigBackup.config_name == config_name).order_by(ConfigBackup.id.desc()).first()
+    
+    if not config:
+        raise HTTPException(status_code=404, detail=f"Config '{config_name}' not found")
+    
+    return {"config_name": config.config_name, "content": config.content}
+
+
+
